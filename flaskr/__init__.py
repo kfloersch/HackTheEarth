0295f77ecfca0ac6a106f6b32916628126a9a6c7
jellihan <han.ang@husky.neu.edu>
2020-05-17 11:57:56 -0400

    added html, css, and .gitignore

---
+++ flaskr/__init__.py
@@ -0,0 +1,43 @@
+import os
+
+from flask import Flask, render_template
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that says hello
+    @app.route('/')
+    def home():
+        return render_template('index.html')
+
+    @app.route('/bottle')
+    def bottle():
+        return render_template('bottle.html')
+
+    @app.route('/resources') 
+    def resources():
+        return render_template('resources.html')
+
+    @app.route('/info')
+    def info():
+        return render_template('info.html')
+    
+    return app
