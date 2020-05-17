2c4980cd47fa00e6e2202fcd2da39868ac9e6d25
jellihan <han.ang@husky.neu.edu>
2020-05-17 15:30:17 -0400

    added upload form and minor css changes

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,6 +1,8 @@
 import os
 
 from flask import Flask, render_template
+from flask import Flask, flash, request, redirect, url_for, send_from_directory
+from werkzeug.utils import secure_filename
 
 def create_app(test_config=None):
     # create and configure the app
@@ -23,10 +25,41 @@ def create_app(test_config=None):
     except OSError:
         pass
 
+    UPLOAD_FOLDER = '../HackTheEarth'
+    ALLOWED_EXTENSIONS = {'jpg'}
+
+    #app = Flask(__name__)
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+    def allowed_file(filename):
+        return '.' in filename and \
+            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
     # a simple page that says hello
-    @app.route('/')
+    @app.route('/', methods=['GET', 'POST'])
     def home():
+        if request.method == 'POST':
+        # check if the post request has the file part
+            if 'file' not in request.files:
+                flash('No file part')
+                return redirect(request.url)
+            file = request.files['file']
+            # if user does not select file, browser also
+            # submit an empty part without filename
+            if file.filename == '':
+                flash('No selected file')
+                return redirect(request.url)
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                return redirect(url_for('uploaded_file',
+                                        filename=filename))
         return render_template('index.html')
+
+    @app.route('/ImageAI_Program/<filename>')
+    def uploaded_file(filename):
+        return send_from_directory(app.config['UPLOAD_FOLDER'],
+                                filename)
 
     @app.route('/bottle')
     def bottle():
@@ -41,3 +74,4 @@ def create_app(test_config=None):
         return render_template('info.html')
     
     return app
+
